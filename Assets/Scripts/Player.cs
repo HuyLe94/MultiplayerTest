5aae51bd72f42529d6044afabbc86cec87969f2b
HuyLe94 <70664981+HuyLe94@users.noreply.github.com>
2021-02-28 15:32:35 -0800

    Rotate and shooting
    Reload need fixing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,17 +10,19 @@ public class Player : MonoBehaviourPun
     private bool moved = false;
     public Camera selfCam;
     public float atk = 10;
-    public float hp =20;
+    public float hp = 20;
     public float speed = 1;
     public float ammo = 3;
-    public float rechargeRate =3;
-    public float bulletSpeed =1;
+    public float rechargeRate = 3;
+    public float bulletSpeed = 5;
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform bulletSpot;
 
     public PhotonView pView;
 
     private void Awake()
     {
-        
+
     }
     private void Start()
     {
@@ -29,11 +31,25 @@ public class Player : MonoBehaviourPun
     private void Update()
     {
         moving();
+        followMouse();
+        reload();
+        //Debug.Log(Mathf.Sqrt(rb.velocity.x*rb.velocity.x + rb.velocity.y*rb.velocity.y));
     }
 
     private void checkInputs()
     {
-        
+        if (Input.GetMouseButtonDown(0) && moved)
+        {
+            var mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            mousePosition = mousePosition - transform.position;
+            rb.velocity = new Vector2(mousePosition.x, mousePosition.y).normalized * speed;
+            
+        }
+        if (Input.GetMouseButtonDown(1) && ammo > 0)
+        {
+            shoot();
+        }
     }
 
     private void moving()
@@ -45,14 +61,47 @@ public class Player : MonoBehaviourPun
                 var mousePosition = Input.mousePosition;
                 mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
                 mousePosition = mousePosition - transform.position;
-                rb.velocity = new Vector2(mousePosition.x/(Mathf.Sqrt((mousePosition.x* mousePosition.x) +(mousePosition.y* mousePosition.y)))*speed,
-                                          mousePosition.y / (Mathf.Sqrt((mousePosition.x * mousePosition.x) + (mousePosition.y * mousePosition.y)))*speed);
+                rb.velocity = new Vector2(mousePosition.x / (Mathf.Sqrt((mousePosition.x * mousePosition.x) + (mousePosition.y * mousePosition.y))) * speed,
+                                          mousePosition.y / (Mathf.Sqrt((mousePosition.x * mousePosition.x) + (mousePosition.y * mousePosition.y))) * speed);
             }
-            moved = false;
+            moved = true;
         }
         else
         {
             checkInputs();
+        }
+    }
+
+    private void followMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y-transform.position.y);
+        transform.up = direction;
+    }
+
+    private void shoot()
+    {
+        
+            Debug.Log("Shooting");
+            GameObject a =Instantiate(bullet, bulletSpot.position, Quaternion.identity);
+            a.GetComponent<BulletScript>().parent = gameObject;
+            a.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.mousePosition.x, Input.mousePosition.y).normalized * bulletSpeed;
+            ammo--;
+            //Debug.Log(ammo);
+    }
+
+    private void reload()
+    {
+        while (ammo < 3)
+        {
+            rechargeRate -= 1 * Time.deltaTime;
+            Debug.Log(rechargeRate);
+            if (rechargeRate <= 0)
+            {
+                ammo++;
+                rechargeRate = 3;
+            }
         }
     }
 }
